a2c2fcc25aec44f9b8b34fd8bb41d1fa854e8fcb
Carlos Bruno <cbcarlos08@gmail.com>
2019-02-18 12:22:57 -0400

    Aula 43. Identificando os componentes no Código

--- app/src/main/java/com/example/appaeroporto/MainActivity.java
+++ app/src/main/java/com/example/appaeroporto/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.appaeroporto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imagem;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imagem =  findViewById( R.id.minhaImagem );
+        texto  =  findViewById( R.id.meuTexto );
+
+        texto.setText( "Toque para continuar" );
+        imagem.setScaleX( -1f ); //inverteu para outro lado
     }
 }
